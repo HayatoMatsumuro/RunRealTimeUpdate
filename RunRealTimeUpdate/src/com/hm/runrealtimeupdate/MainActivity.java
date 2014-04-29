6399d0654eff2e9270c90c5da5fa08e11b7510cc
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-04-29 13:12:48 +0900

    【追加】【メイン画面】新規大会登録ボタンのリスナー作成

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/MainActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/MainActivity.java
@@ -3,13 +3,28 @@ package com.hm.runrealtimeupdate;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // ‘å‰ï“o˜^ƒ{ƒ^ƒ“
+        // TODO: ‘å‰ï“o˜^”‚ª5ˆÈã‚Ìê‡‚ÍA”ñ•\¦
+        
+        Button entryBtn = (Button)findViewById(R.id.id_main_btn_entry);
+        entryBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO ‘å‰ï“o˜^‰æ–Ê‚É‘JˆÚ
+			}
+		});
     }
 
 
