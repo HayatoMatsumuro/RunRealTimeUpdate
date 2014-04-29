9d0ecd61eb68e020a58cd1da1f03d6aa780ffb5d
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-04-29 18:04:36 +0900

    【追加】【大会登録】戻るボタンの処理追加

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceEntryActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceEntryActivity.java
@@ -1,7 +1,11 @@
 package com.hm.runrealtimeupdate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class RaceEntryActivity extends Activity {
 	
@@ -9,5 +13,17 @@ public class RaceEntryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_raceentry);
+        
+        // –ß‚éƒ{ƒ^ƒ“
+        Button backBtn = (Button)findViewById(R.id.id_raceentry_btn_back);
+        backBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// ‘å‰ï“o˜^‰æ–Ê‘JˆÚ
+				Intent intent = new Intent(RaceEntryActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
