8592435c8d5f2cce03704a39bc9bcd39f7321179
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-05-10 21:00:21 +0900

    【追加】【大会詳細】大会名、開催日、開催地表示

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceDetailActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceDetailActivity.java
@@ -1,7 +1,12 @@
 package com.hm.runrealtimeupdate;
 
+import com.hm.runrealtimeupdate.logic.DataBaseAccess;
+import com.hm.runrealtimeupdate.logic.DataBaseRaceInfo;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RaceDetailActivity extends Activity {
 	
@@ -11,6 +16,24 @@ public class RaceDetailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_racedetail);
+        
+        // 大会情報取得
+        Intent intent = getIntent();
+        String raceId = intent.getStringExtra(STR_INTENT_RACEID);
+        DataBaseRaceInfo dbRaceInfo = DataBaseAccess.getRaceInfoByRaceId(getContentResolver(), raceId);
+
+        // 大会名表示
+        TextView raceNameTextView = (TextView)findViewById(R.id.id_racedetail_txt_racename);
+        raceNameTextView.setText(dbRaceInfo.getRaceName());
+        
+        // 大会日
+        TextView raceDateTextView = (TextView)findViewById(R.id.id_racedetail_txt_racedate);
+        raceDateTextView.setText(dbRaceInfo.getRaceDate());
+        
+        // 開催地
+        TextView raceLocationTextView = (TextView)findViewById(R.id.id_racedetail_txt_racelocation);
+        raceLocationTextView.setText(dbRaceInfo.getRaceLocation());
+        
 	}
 
 }
--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/DataBaseAccess.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/DataBaseAccess.java
@@ -42,7 +42,7 @@ public class DataBaseAccess {
 	/**
 	 * 全大会情報を取得する
 	 * @param contentResolver
-	 * @return
+	 * @return 大会情報。存在しない場合は空のリスト。
 	 */
 	public static List<DataBaseRaceInfo> getAllRaceInfo( ContentResolver contentResolver ){
 		
@@ -53,25 +53,54 @@ public class DataBaseAccess {
 		Cursor c = contentResolver.query(RaceProvider.URI_DB, projection, null, null, null);
 		
 		while(c.moveToNext()){
-			// データ取り出し
-			String id = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACEID));
-			String name = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACENAME));
-			String date = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACEDATE));
-			String location = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACELOCATION));
-			String updateFlg = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_UPDATEFLG));
-			
 			// データ設定
-			DataBaseRaceInfo info = new DataBaseRaceInfo();
-			info.setRaceId(id);
-			info.setRaceName(name);
-			info.setRaceDate(date);
-			info.setRaceLocation(location);
-			info.setUpdateFlg(updateFlg);
+			DataBaseRaceInfo info = getRaceInfoByCursor(c);
 			list.add(info);
 		}
 
 		c.close();
 		
 		return list;
+	}
+	
+	/**
+	 * 大会IDから大会情報を取得する
+	 * @param contentResolver
+	 * @param raceId 大会ID
+	 * @return　大会情報。存在しない場合はnull
+	 */
+	public static DataBaseRaceInfo getRaceInfoByRaceId( ContentResolver contentResolver, String raceId ){
+		
+		DataBaseRaceInfo raceInfo = null;
+		
+		String[] projection = {RaceProvider.STR_DB_COLUMN_RACEID, RaceProvider.STR_DB_COLUMN_RACENAME, RaceProvider.STR_DB_COLUMN_RACEDATE, RaceProvider.STR_DB_COLUMN_RACELOCATION, RaceProvider.STR_DB_COLUMN_UPDATEFLG};
+		String selection = RaceProvider.STR_DB_COLUMN_RACEID + "='" + raceId+"'";
+		
+		Cursor c = contentResolver.query(RaceProvider.URI_DB, projection, selection, null, null);
+		
+		while(c.moveToNext()){
+			raceInfo = getRaceInfoByCursor(c);
+		}
+		return raceInfo;
+	}
+	
+	private static DataBaseRaceInfo getRaceInfoByCursor(Cursor c){
+		
+		// データ取り出し
+		String id = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACEID));
+		String name = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACENAME));
+		String date = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACEDATE));
+		String location = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_RACELOCATION));
+		String updateFlg = c.getString(c.getColumnIndex(RaceProvider.STR_DB_COLUMN_UPDATEFLG));
+		
+		// データ設定
+		DataBaseRaceInfo info = new DataBaseRaceInfo();
+		info.setRaceId(id);
+		info.setRaceName(name);
+		info.setRaceDate(date);
+		info.setRaceLocation(location);
+		info.setUpdateFlg(updateFlg);
+		
+		return info;
 	}
 }
