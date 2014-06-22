c58007fbfab65424651da9840ee7674821988e99
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-06-22 23:16:36 +0900

    【大会】大会情報取得処理の追加

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceTabActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceTabActivity.java
@@ -1,6 +1,10 @@
 package com.hm.runrealtimeupdate;
 
+import com.hm.runrealtimeupdate.logic.Logic;
+import com.hm.runrealtimeupdate.logic.RaceInfo;
+
 import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.TabHost;
@@ -14,6 +18,18 @@ public class RaceTabActivity extends TabActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tabactivity_race);
 		
+		// 大会情報取得
+        Intent intent = getIntent();
+        String raceId = intent.getStringExtra(STR_INTENT_RACEID);
+        RaceInfo raceInfo = Logic.getRaceInfo(getContentResolver(), raceId);
+        
+        // 大会情報が取得できないなら、エラー画面
+        if( raceInfo == null ){
+        	Intent intentErr = new Intent(RaceTabActivity.this, ErrorActivity.class);
+        	intentErr.putExtra(ErrorActivity.STR_INTENT_MESSAGE, "大会情報取得に失敗しました。");
+        	return;
+        }
+        
 		// ヘッダー
         RelativeLayout headerLayout = (RelativeLayout)findViewById(R.id.id_race_relative_header);
         headerLayout.setBackgroundColor(getResources().getColor(R.color.maincolor));
