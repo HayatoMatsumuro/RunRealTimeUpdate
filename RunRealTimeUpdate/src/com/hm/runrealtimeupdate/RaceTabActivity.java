dd5873eb029f6d4dab2c927634bbf3ea954e1009
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-06-22 23:13:38 +0900

    【大会】レイアウトの背景色設定

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceTabActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/RaceTabActivity.java
@@ -2,6 +2,7 @@ package com.hm.runrealtimeupdate;
 
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TabHost;
 
 public class RaceTabActivity extends TabActivity {
@@ -13,6 +14,14 @@ public class RaceTabActivity extends TabActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tabactivity_race);
 		
+		// ヘッダー
+        RelativeLayout headerLayout = (RelativeLayout)findViewById(R.id.id_race_relative_header);
+        headerLayout.setBackgroundColor(getResources().getColor(R.color.maincolor));
+        
+        // ボーダー
+        RelativeLayout borderLayout = (RelativeLayout)findViewById(R.id.id_race_relative_border);
+        borderLayout.setBackgroundColor(getResources().getColor(R.color.subcolor));
+        
 		// タブ設定
 		TabHost tabHost = getTabHost();
 		tabHost.setCurrentTab(0);
