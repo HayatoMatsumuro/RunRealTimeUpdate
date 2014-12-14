bcb52e6af8275638d6f1cb33572ea86aa260e6f6
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2014-12-14 13:14:03 +0900

    【修正】メイン画面 コメントなど

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/MainActivity.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/MainActivity.java
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,6 +24,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * メイン画面のActivity
+ * @author Hayato Matsumuro
+ *
+ */
 public class MainActivity extends Activity
 {
 
@@ -37,60 +41,67 @@ public class MainActivity extends Activity
 		ListView raceInfoListView = ( ListView )findViewById( R.id.id_activity_main_body_contents_racelist_listview );
 
 		// リストのアイテム短押し
-		raceInfoListView.setOnItemClickListener( new OnItemClickListener()
-		{
-			@Override
-			public void onItemClick( AdapterView<?> parent, View v, int position, long id )
+		raceInfoListView.setOnItemClickListener(
+			new OnItemClickListener()
 			{
-				// 選択した大会情報を取得する
-				ListView listView = ( ListView )parent;
-				RaceInfo raceInfo = ( RaceInfo )listView.getItemAtPosition( position );
+				@Override
+				public void onItemClick( AdapterView<?> parent, View v, int position, long id )
+				{
+					// 選択した大会情報を取得する
+					ListView listView = ( ListView )parent;
+					RaceInfo raceInfo = ( RaceInfo )listView.getItemAtPosition( position );
 
-				// 画面遷移
-				Intent intent = new Intent( MainActivity.this, RaceTabActivity.class );
-				intent.putExtra( RaceTabActivity.STR_INTENT_RACEID, raceInfo.getRaceId() );
-				intent.putExtra( RaceTabActivity.STR_INTENT_CURRENTTAB, RaceTabActivity.INT_INTENT_VAL_CURRENTTAB_DETAIL );
-				startActivity( intent );
+					// 画面遷移
+					Intent intent = new Intent( MainActivity.this, RaceTabActivity.class );
+					intent.putExtra( RaceTabActivity.STR_INTENT_RACEID, raceInfo.getRaceId() );
+					intent.putExtra( RaceTabActivity.STR_INTENT_CURRENTTAB, RaceTabActivity.INT_INTENT_VAL_CURRENTTAB_DETAIL );
+					startActivity( intent );
+				}
 			}
-		} );
+		);
 
 		// リストのアイテム長押し
-		raceInfoListView.setOnItemLongClickListener( new OnItemLongClickListener()
-		{
-			@Override
-			public boolean onItemLongClick( AdapterView<?> parent, View v, int position, long id )
+		raceInfoListView.setOnItemLongClickListener(
+			new OnItemLongClickListener()
 			{
+				@Override
+				public boolean onItemLongClick( AdapterView<?> parent, View v, int position, long id )
+				{
+					// 選択した大会情報を取得する
+					ListView listView = ( ListView )parent;
+					RaceInfo raceInfo = ( RaceInfo )listView.getItemAtPosition( position );
 
-				// 選択した大会情報を取得する
-				ListView listView = ( ListView )parent;
-				RaceInfo raceInfo = ( RaceInfo )listView.getItemAtPosition( position );
-
-				// 削除ダイアログ表示
-				InfoDialog<RaceInfo> raceDeleteInfoDialog = new InfoDialog<RaceInfo>( raceInfo, new RaceDeleteButtonCallbackImpl() );
-				raceDeleteInfoDialog.onDialog(
+					// 削除ダイアログ表示
+					InfoDialog<RaceInfo> raceDeleteInfoDialog = new InfoDialog<RaceInfo>( raceInfo, new RaceDeleteButtonCallbackImpl() );
+					raceDeleteInfoDialog.onDialog(
 						MainActivity.this,
 						getString( R.string.str_dialog_title_deleterace ),
 						createDialogMessage( raceInfo ),
 						getString( R.string.str_dialog_msg_DEL ),
 						getString( R.string.str_dialog_msg_NG )
-				);
+					);
 
-				return true;
+					return true;
+				}
 			}
-		});
+		);
 
 		// 大会登録ボタン
 		Button entryBtn = ( Button )findViewById( R.id.id_activity_main_header_entry_button );
-		entryBtn.setOnClickListener( new OnClickListener() {
-
-			@Override
-			public void onClick( View v )
+		entryBtn.setOnClickListener(
+			new OnClickListener()
 			{
-				// 大会登録画面遷移
-				Intent intent = new Intent( MainActivity.this, RaceEntryActivity.class );
-				startActivity( intent );
+				@Override
+				public void onClick( View v )
+				{
+					// 大会登録画面遷移
+					Intent intent = new Intent( MainActivity.this, RaceEntryActivity.class );
+					startActivity( intent );
+				}
 			}
-		});
+		);
+
+		return;
 	}
 
     @Override
@@ -115,9 +126,10 @@ public class MainActivity extends Activity
 
 		if( chkRaceInfo != null )
 		{
+			// アラーム停止中
 			if( !CommonLib.isSetUpdateAlarm( MainActivity.this ) )
 			{
-				Log.d( "main", "Alarm Stop, database updateexe");
+				// 速報状態停止
 				Logic.setUpdateOffRaceId( getContentResolver(), chkRaceInfo.getRaceId() );
 				chkRaceInfo.setRaceUpdate( false );
 			}
@@ -130,10 +142,13 @@ public class MainActivity extends Activity
 
 		// 表示状態設定
 		setViewBody( raceInfoListView.getCount() );
+
+		return;
 	}
 
 	/**
-	 * ボディの表示設定
+	 * ボディの表示を設定する
+	 * @param raceInfoNum 大会情報の数
 	 */
 	private void setViewBody( int raceInfoNum )
 	{
@@ -151,12 +166,14 @@ public class MainActivity extends Activity
 			contentsLayout.setVisibility( View.VISIBLE );
 			messageLayout.setVisibility( View.GONE );
 		}
+
+		return;
 	}
 
 	/**
 	 * ダイアログメッセージ作成
 	 * @param raceInfo 大会情報
-	 * @return
+	 * @return ダイアログメッセージ
 	 */
 	private String createDialogMessage( RaceInfo raceInfo )
 	{
@@ -177,11 +194,10 @@ public class MainActivity extends Activity
 	private class RaceDeleteButtonCallbackImpl implements InfoDialog.ButtonCallback<RaceInfo>
 	{
 		@Override
-		public void onClickPositiveButton(DialogInterface dialog, int which, RaceInfo info)
+		public void onClickPositiveButton( DialogInterface dialog, int which, RaceInfo info )
 		{
-			// ポジティブボタン押し
-			// 速報中でないなら削除する
-			if( !info.isRaceUpdate())
+			// 速報中でない
+			if( !info.isRaceUpdate() )
 			{
 				// 大会削除
 				Logic.deleteRaceInfo( getContentResolver(), info.getRaceId() );
@@ -196,17 +212,19 @@ public class MainActivity extends Activity
 		        setViewBody( raceInfoListView.getCount() );
 
 				Toast.makeText( MainActivity.this, "削除しました", Toast.LENGTH_SHORT ).show();
-			
 			}
 			else
 			{
 				Toast.makeText( MainActivity.this, "速報中のため、削除できません。", Toast.LENGTH_SHORT ).show();
 			}
+
+			return;
 		}
 
 		@Override
 		public void onClickNegativeButton( DialogInterface dialog, int which, RaceInfo info )
 		{
+			return;
 		}
 	}
 
@@ -217,13 +235,23 @@ public class MainActivity extends Activity
 	 */
 	private class RaceListAdapter extends ArrayAdapter<RaceInfo>
 	{
-		LayoutInflater inflater;
+		/**
+		 * レイアウトインフレータ
+		 */
+		LayoutInflater m_Inflater;
 
-		public RaceListAdapter( Context context, List<RaceInfo> objects )
+		/**
+		 * コンストラクタ
+		 * @param context　コンテキスト
+		 * @param raceInfoList 大会情報リスト
+		 */
+		public RaceListAdapter( Context context, List<RaceInfo> raceInfoList )
 		{
-			super( context, 0, objects );
+			super( context, 0, raceInfoList );
 
-			this.inflater = ( LayoutInflater )context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+			m_Inflater = ( LayoutInflater )context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+
+			return;
 		}
 
 		@Override
@@ -231,18 +259,19 @@ public class MainActivity extends Activity
 		{
 			if( convertView == null )
 			{
-				convertView = this.inflater.inflate( R.layout.list_item_raceinfo, parent, false );
+				convertView = m_Inflater.inflate( R.layout.list_item_raceinfo, parent, false );
 			}
 
-			RelativeLayout raceUpdateLayout = ( RelativeLayout )convertView.findViewById( R.id.id_list_item_raceinfo_update_layout );
-
-			TextView raceNameTextView = ( TextView )convertView.findViewById( R.id.id_list_item_raceinfo_race_name_textview );
-
+			// 大会情報取得
 			RaceInfo raceInfo = getItem( position );
 
+			// 大会名表示
+			TextView raceNameTextView = ( TextView )convertView.findViewById( R.id.id_list_item_raceinfo_race_name_textview );
 			raceNameTextView.setText( raceInfo.getRaceName() );
 
-			// 速報中の大会ならば、速報中と表示
+			// 速報中の表示
+			RelativeLayout raceUpdateLayout = ( RelativeLayout )convertView.findViewById( R.id.id_list_item_raceinfo_update_layout );
+
 			if(raceInfo.isRaceUpdate())
 			{
 				raceUpdateLayout.setVisibility( View.VISIBLE );
