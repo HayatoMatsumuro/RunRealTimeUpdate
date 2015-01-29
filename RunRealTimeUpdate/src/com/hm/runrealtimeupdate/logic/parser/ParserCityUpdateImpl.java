4d739b42fec82779f8eeb2974d65d7e6292cdfaa
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2015-01-29 23:51:05 +0900

    【追加】都市型マラソンのパーサー追加

---
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/parser/ParserCityUpdateImpl.java
@@ -0,0 +1,25 @@
+package com.hm.runrealtimeupdate.logic.parser;
+
+import java.util.List;
+
+public class ParserCityUpdateImpl implements ParserUpdate
+{
+	@Override
+	public ParserRaceInfo getRaceInfo( String url, String raceId ) throws ParserException
+	{
+		return null;
+	}
+
+	@Override
+	public ParserRunnerInfo getRunnerInfo( String url, String raceId, String no ) throws ParserException
+	{
+		return null;
+	}
+
+	@Override
+	public List<ParserRunnerInfo> searchRunnerInfoByName( String url, String raceId, String sei, String mei ) throws ParserException
+	{
+		return null;
+	}
+
+}
