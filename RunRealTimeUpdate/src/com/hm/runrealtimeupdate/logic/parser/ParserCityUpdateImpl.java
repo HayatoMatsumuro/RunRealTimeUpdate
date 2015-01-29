7a52eabc207c360b61b6853138b68769171f1cda
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2015-01-30 00:02:06 +0900

    【追加】都市型マラソンパーサー 大会情報取得処理追加

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/parser/ParserCityUpdateImpl.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/parser/ParserCityUpdateImpl.java
@@ -1,13 +1,49 @@
 package com.hm.runrealtimeupdate.logic.parser;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class ParserCityUpdateImpl implements ParserUpdate
 {
 	@Override
 	public ParserRaceInfo getRaceInfo( String url, String raceId ) throws ParserException
 	{
-		return null;
+		try
+		{
+			Connection connection = Jsoup.connect( url );
+
+			if( connection == null )
+			{
+				throw new ParserException( "接続に失敗しました。" );
+			}
+
+			Document doc = connection.get();
+			if( doc == null )
+			{
+				throw new ParserException( "URLが不正です。" );
+			}
+
+			// 大会情報はhtmlから取得できない
+			// 接続できれば、大会登録可能とする
+			// 大会情報設定
+			ParserRaceInfo parserRaceInfo = new ParserRaceInfo();
+
+			return parserRaceInfo;
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+			throw new ParserException( "IOException." );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			throw new ParserException( "Exception." );
+		}
 	}
 
 	@Override
