9cbf1eba4b657b4fa847216d634c53b69f43f3e0
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2015-01-31 21:04:36 +0900

    【追加】都市型マラソンパーサー 選手情報取得処理

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/parser/ParserCityUpdateImpl.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/logic/parser/ParserCityUpdateImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ParserCityUpdateImpl implements ParserUpdate
 {
@@ -49,13 +51,124 @@ public class ParserCityUpdateImpl implements ParserUpdate
 	@Override
 	public ParserRunnerInfo getRunnerInfo( String url, String pass, String no ) throws ParserException
 	{
-		return null;
+		try
+		{
+			String runnerInfoUrl = createRunnerInfoURL( url, pass, no );
+
+			Connection connection = Jsoup.connect( runnerInfoUrl );
+			if( connection == null )
+			{
+				throw new ParserException( "接続に失敗しました。" );
+			}
+
+			Document doc = connection.get();
+			if( doc == null )
+			{
+				throw new ParserException( "データ取得に失敗しました。" );
+			}
+
+			// 個人情報取得
+			Elements contentsBlock = doc.select( "div#contentsBlock" );
+			if( ( contentsBlock == null ) || ( contentsBlock.isEmpty() ) )
+			{
+				throw new ParserException( "データ取得に失敗しました。" );
+			}
+
+			Element contentsBlockElement = contentsBlock.get( 0 );
+			if( contentsBlockElement == null )
+			{
+				throw new ParserException( "ゼッケンNOが不正です。" );
+			}
+
+			Elements ddElements = contentsBlockElement.getElementsByTag( "dd" );
+			if( ( ddElements == null ) || ( ddElements.isEmpty() ) )
+			{
+				throw new ParserException( "ゼッケンNOが不正です。" );
+			}
+
+			ParserRunnerInfo parserRunnerInfo = new ParserRunnerInfo();
+
+			// ゼッケンNo取得
+			StringBuilder strNo = new StringBuilder( ddElements.get( 2 ).text() );
+			parserRunnerInfo.number = strNo.toString();
+
+			// 名前取得
+			StringBuilder strName = new StringBuilder( ddElements.get( 3 ).text() );
+			parserRunnerInfo.name = strName.toString();
+
+			// 部門取得
+			StringBuilder strSection = new StringBuilder( ddElements.get( 4 ).text() );
+			parserRunnerInfo.section = strSection.toString();
+
+			// スプリット情報取得
+			Elements listAdWrapper = doc.select( "div#listAdWrapper" );
+			if( ( listAdWrapper == null ) || ( listAdWrapper.isEmpty() ) )
+			{
+				throw new ParserException( "スプリットが不正です。" );
+			}
+
+			Element mainBlockElement = listAdWrapper.get(0);
+			if( mainBlockElement == null )
+			{
+				throw new ParserException( "スプリットが不正です。" );
+			}
+
+			Elements trElements = mainBlockElement.getElementsByTag( "tr" );
+			trElements.remove( 0 );
+
+			for( Element trElement : trElements )
+			{
+				Elements tdElements = trElement.getElementsByTag( "td" );
+
+				ParserRunnerInfo.TimeInfo timeInfo = new ParserRunnerInfo().new TimeInfo();
+
+				timeInfo.point = tdElements.get( 0 ).text();
+
+				String[] splits = tdElements.get( 1 ).text().split("　");
+				timeInfo.split = splits[ 0 ];
+				timeInfo.lap = tdElements.get( 2 ).text();
+				timeInfo.currentTime = tdElements.get( 3 ).text();
+
+				parserRunnerInfo.timeList.add( timeInfo );
+			}
+
+			return parserRunnerInfo;
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+			throw new ParserException( "IOException." );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			throw new ParserException( "Exception." );
+		}
 	}
 
 	@Override
 	public List<ParserRunnerInfo> searchRunnerInfoByName( String url, String pass, String sei, String mei ) throws ParserException
 	{
 		return null;
+	}
+
+	/**
+	 * ランナー情報を取得するURLを作成する
+	 * @param url アップデートサイトURL
+	 * @param raceId　大会ID
+	 * @param no　ゼッケンNo.
+	 * @return　選手情報アクセスURL
+	 */
+	private static String createRunnerInfoURL( String url, String raceId, String no )
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append( url );
+		builder.append( raceId );
+		builder.append( "/numberfile/" );
+		builder.append( no );
+		builder.append( ".html" );
+
+		return builder.toString();
 	}
 
 }
