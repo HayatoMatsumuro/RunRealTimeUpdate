8154f3c09a95972f11d76c82b7b410cbd97df7df
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2015-02-15 18:26:07 +0900

    【修正】Common public を削除

--- RunRealTimeUpdate/src/com/hm/runrealtimeupdate/Common.java
+++ RunRealTimeUpdate/src/com/hm/runrealtimeupdate/Common.java
@@ -5,42 +5,42 @@ package com.hm.runrealtimeupdate;
  * @author Hayato Matsumuro
  *
  */
-public class Common
+class Common
 {
 	/**
 	 * サービスの起動間隔
 	 */
-	public static final int INT_SERVICE_INTERVAL = 180000;
+	static final int INT_SERVICE_INTERVAL = 180000;
 
 	/**
 	 * 選手情報取得のネットアクセス間隔
 	 */
-	public static final int INT_SERVICE_NETGET_INTERVAL = 700;
+	static final int INT_SERVICE_NETGET_INTERVAL = 700;
 
 	/**
 	 * 最後の更新から自動停止までの更新確認回数( 7時間 )
 	 */
-	public static final int INT_COUNT_AUTOSTOP_LASTUPDATE = 25200000 / INT_SERVICE_INTERVAL;
+	static final int INT_COUNT_AUTOSTOP_LASTUPDATE = 25200000 / INT_SERVICE_INTERVAL;
 
 	/**
 	 * 自動開始から停止までの更新確認回数( 24時間 )
 	 */
-	public static final int INT_COUNT_REGULARSTOP = 86400000 / INT_SERVICE_INTERVAL;
+	static final int INT_COUNT_REGULARSTOP = 86400000 / INT_SERVICE_INTERVAL;
 
 	/**
 	 * 手動更新の更新確認回数( 1回のみ )
 	 */
-	public static final int INT_COUNT_MANUALSTOP = 1;
+	static final int INT_COUNT_MANUALSTOP = 1;
 
 	/**
 	 * リクエストコード 更新定期
 	 */
-	public static final int INT_REQUESTCODE_UPDATEPERIODIC = 1;
+	static final int INT_REQUESTCODE_UPDATEPERIODIC = 1;
 
 	/**
 	 * リクエストコード 更新予約
 	 */
-	public static final int INT_REQUESTCODE_UPDATERESERVE = 2;
+	static final int INT_REQUESTCODE_UPDATERESERVE = 2;
 
 	/**
 	 * NEWの表示時間( ms )
